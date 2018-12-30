23914a2606e839c1348430b61eac0487272d6435
lightyears <lightyears1998@hotmail.com>
2018-12-30 20:36:54 +0800

    画图

--- GeometrySketchpad/Resource.h
+++ GeometrySketchpad/Resource.h
@@ -1,19 +1,23 @@
 ﻿//{{NO_DEPENDENCIES}}
-// 生成的 Microsoft Visual C++ 包含文件。
-// 由 GeometrySketchpad.rc 使用
+// Microsoft Visual C++ generated include file.
+// Used by GeometrySketchpad.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_GeometrySketchpadTYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_GeometrySketchpadTYPE       130
+#define ID_DRAW_POINT                   32772
+#define ID_DRAW_LINE                    32774
+#define ID_SELECTON                     32777
+#define ID_MOUSE_SELECTON               32777
 
-// 新对象的下一组默认值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
