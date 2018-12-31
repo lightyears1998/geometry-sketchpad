6c68b9562f41c0c58b0b0ff1586884a358b5d161
lightyears <lightyears1998@hotmail.com>
2018-12-31 10:36:15 +0800

    Update resource.h

--- GeometrySketchpad/resource.h
+++ GeometrySketchpad/resource.h
@@ -21,13 +21,15 @@
 #define ID_32786                        32786
 #define ID_ADD_POINT                    32787
 #define ID_ADD_SEGMENT                  32788
+#define ID_32789                        32789
+#define ID_DEBUG                        32790
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        313
-#define _APS_NEXT_COMMAND_VALUE         32789
+#define _APS_NEXT_COMMAND_VALUE         32791
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
